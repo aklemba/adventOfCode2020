f15cd93def894cda4a75b644b85e374289d1a260
Adam Klemba <adam.a.klemba@gmail.com>
2020-12-09 20:47:59 +0100

    Day 1 Part 2

--- src/main/kotlin/day_1/main.kt
+++ src/main/kotlin/day_1/main.kt
@@ -5,17 +5,34 @@ const val INPUT_FILE_NAME = "src/main/kotlin/day_1/input.txt"
 const val SUM_TO_FIND = 2020
 
 fun main() {
-    val numberList = getInputAsAListOfLines().parseToIntList()
-    println(fetchTheResult(numberList))
+    val listOfNumbers = getInputAsAListOfLines().parseToIntList()
+
+    println(fetchForResultOfPart1(listOfNumbers))
+
+    println(fetchForResultOfPart2(listOfNumbers))
 }
 
-fun fetchTheResult(listOfNumbers: List<Int>): Int {
+fun fetchForResultOfPart1(listOfNumbers: List<Int>): Int {
     for (i in listOfNumbers.indices) {
         for (j in i + 1 until listOfNumbers.size) {
 
             if (listOfNumbers[i] + listOfNumbers[j] == SUM_TO_FIND) {
                 println("First number: ${listOfNumbers[i]} \nSecond number: ${listOfNumbers[j]}")
                 return listOfNumbers[i] * listOfNumbers[j]
+            }
+        }
+    }
+    return -1
+}
+
+fun fetchForResultOfPart2(listOfNumbers: List<Int>): Int {
+    for (i in listOfNumbers.indices) {
+        for (j in i + 1 until listOfNumbers.size) {
+            for (k in j + 1 until listOfNumbers.size) {
+                if (listOfNumbers[i] + listOfNumbers[j] + listOfNumbers[k] == SUM_TO_FIND) {
+                    println("First number: ${listOfNumbers[i]} \nSecond number: ${listOfNumbers[j]} \nThird number: ${listOfNumbers[k]}")
+                    return listOfNumbers[i] * listOfNumbers[j] * listOfNumbers[k]
+                }
             }
         }
     }
